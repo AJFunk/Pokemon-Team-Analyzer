d9697fec917eee12a22cfc17ec684318b725ced4
AJ Funk <aj.funk@onmego.com>
2014-09-02 01:14:56 -0700

    Cleaned up pokedex

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
     //Constantly update Type Chart
    setInterval(function(){update();}, 100);
+   /*$(document).on("keypress", function (e) {
+    	update();
+	});
+
+   $(document).on("click", function (e) {
+    	update();
+	});*/
 
     //Fix sizing on top row
     $(window).on('resize', function(){
